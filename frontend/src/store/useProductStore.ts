62fe1dc1f40dfe876654879b8b9a9e5982145504
Phsithzz <phoosith2548@gmail.com>
2026-03-01 19:18:48 +0700

    config frontend

--- frontend/src/store/useProductStore.ts
+++ frontend/src/store/useProductStore.ts
@@ -6,7 +6,7 @@ import * as productType from "../types/productType";
 import type { ApiResponse } from "../types/api";
 import toast from "react-hot-toast";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://storeboard.onrender.com";
 
 export const useProductStore = create<productType.ProductStore>((set, get) => ({
   products: [],
